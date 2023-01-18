f17d1f3b495000e3c1d0ddd948123e27c11bbfcd
Jody VanHoose <jodyvanhoose80@gmail.com>
2023-01-17 19:00:08 -0500

    JS to hide/show sections & display rating on thank you section

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let ratingBtns = document.querySelectorAll('.rating__num');
+let rating = document.getElementById('thanks-rating');
 
+
+// Selecting active state for rating button when clicked as well as assigning the rating number on the thank you section
 ratingBtns.forEach(btn => {
   btn.addEventListener('click', function(){
     ratingBtns.forEach(btn => btn.classList.remove('active'));
     this.classList.add('active');
+    rating.innerHTML = btn.innerHTML;
   })
 })
+
+// Displays thank you section and hides rating section when clicked
+function submitRating(){
+  const ratingSection = document.getElementById('rating');
+  const thanksSection = document.getElementById('thanks');
+  ratingSection.style.display = 'none';
+  thanksSection.style.display = 'flex';
+}
+
